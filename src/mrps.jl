492b5cdc2b433bd013882f8903db8ff92d30e1c9
Patrick Bouffard <231346+pbouffard@users.noreply.github.com>
2021-01-12 04:14:32 -0800

    MRP docstring fix (#136)

--- src/mrps.jl
+++ src/mrps.jl
@@ -3,7 +3,7 @@
 
 Modified Rodrigues Parameter. Is a 3D parameterization of attitude, and is a sterographic
 projection of the 4D unit sphere onto the plane tangent to the negative real pole. They
-have a singularity at θ = ±180°.
+have a singularity at θ = ±360°.
 
 # Constructors
 MRP(x, y, z)
