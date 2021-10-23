6d5bbf43dc622bfa4af3ac99db2efbe127dda555
hyrodium <hyrodium@gmail.com>
2021-10-23 23:57:13 +0900

    update tests for eigen

--- test/eigen.jl
+++ test/eigen.jl
@@ -6,10 +6,19 @@
                  RotX, RotY, RotZ,
                  RotXY, RotYZ, RotZX, RotXZ, RotYX, RotZY)
 
-    for type in all_types
-        R = rand(type)
+    for T in all_types, F in (one, rand)
+        R = F(T)
         λs = eigvals(R)
         vs = eigvecs(R)
+        E = eigen(R)
+        v1 = vs[:,1]
+        v2 = vs[:,2]
+        v3 = vs[:,3]
         @test R * vs ≈ transpose(λs) .* vs
+        @test norm(v1) ≈ 1
+        @test norm(v2) ≈ 1
+        @test norm(v3) ≈ 1
+        @test E.values == λs
+        @test E.vectors == vs
     end
 end
