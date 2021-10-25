b12554cb1046c2c9f599a48b182c8ff382902684
hyrodium <hyrodium@gmail.com>
2021-10-25 23:51:06 +0900

    update 3d eigen

--- src/eigen.jl
+++ src/eigen.jl
@@ -1,14 +1,15 @@
+# 3D
 function LinearAlgebra.eigvals(R::Rotation{3})
     θ = rotation_angle(R)
-    return SVector(exp(θ*im), exp(-θ*im), 1)
+    return SVector(exp(-θ*im), exp(θ*im), 1)
 end
 
 function LinearAlgebra.eigvecs(R::Rotation{3})
     n3 = normalize(rotation_axis(R))
     n1 = normalize(perpendicular_vector(n3))
     n2 = normalize(n3×n1)
-    v1 = normalize(n1*im + n2)
-    v2 = normalize(n1 + n2*im)
+    v1 = normalize(n1 + n2*im)
+    v2 = normalize(n1*im + n2)
     v3 = n3
     return hcat(v1,v2,v3)
 end
