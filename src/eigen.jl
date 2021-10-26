ff7b0388e8bca17bb3fd936f8ef462a662387158
Yuto Horikawa <hyrodium@gmail.com>
2021-10-26 21:08:07 +0900

    Add support for calculating eigenvalues and eigenvectors (#170)
    * add support for eigen values and eigen vectors
    
    * update methods for eigen
    
    * update tests for eigen
    
    * update 3d eigen
    
    * add eigen methods for 2d
    
    * update tests for eigen
    
    * update tests for Julia 1.0
    
    * update tests for Julia 1.0
    
    * fix bug in eigen test
    
    * fix bug in eigen test
    
    * update performance

---
+++ src/eigen.jl
@@ -0,0 +1,42 @@
+# 3D
+function LinearAlgebra.eigvals(R::Rotation{3})
+    θ = rotation_angle(R)
+    return SVector(exp(-θ*im), exp(θ*im), 1)
+end
+
+function LinearAlgebra.eigvecs(R::Rotation{3})
+    n3 = normalize(rotation_axis(R))
+    n1 = normalize(perpendicular_vector(n3))
+    n2 = normalize(n3×n1)
+    v1 = normalize(n1 + n2*im)
+    v2 = normalize(n1*im + n2)
+    v3 = n3
+    return hcat(v1,v2,v3)
+end
+
+function LinearAlgebra.eigen(R::Rotation{3})
+    return eigen(AngleAxis(R))
+end
+
+function LinearAlgebra.eigen(R::AngleAxis)
+    λs = eigvals(R)
+    vs = eigvecs(R)
+    return Eigen(λs, vs)
+end
+
+
+# 2D
+function LinearAlgebra.eigvals(R::Rotation{2})
+    θ = rotation_angle(R)
+    return SVector(exp(-θ*im), exp(θ*im))
+end
+
+function LinearAlgebra.eigvecs(R::Rotation{2})
+    return @SMatrix [1/√2 1/√2;im/√2 -im/√2]
+end
+
+function LinearAlgebra.eigen(R::Rotation{2})
+    λs = eigvals(R)
+    vs = eigvecs(R)
+    return Eigen(λs, vs)
+end
---
+++ test/eigen.jl
@@ -0,0 +1,57 @@
+@testset "Eigen_3D" begin
+    all_types = (RotMatrix{3}, AngleAxis, RotationVec,
+                 UnitQuaternion, RodriguesParam, MRP,
+                 RotXYZ, RotYZX, RotZXY, RotXZY, RotYXZ, RotZYX,
+                 RotXYX, RotYZY, RotZXZ, RotXZX, RotYXY, RotZYZ,
+                 RotX, RotY, RotZ,
+                 RotXY, RotYZ, RotZX, RotXZ, RotYX, RotZY)
+    oneaxis_types = (RotX, RotY, RotZ)
+
+    @testset "$(T)" for T in all_types, F in (one, rand)
+        R = F(T)
+        λs = eigvals(R)
+        vs = eigvecs(R)
+        E = eigen(R)
+        v1 = vs[:,1]
+        v2 = vs[:,2]
+        v3 = vs[:,3]
+        @test R * vs ≈ transpose(λs) .* vs
+        @test norm(v1) ≈ 1
+        @test norm(v2) ≈ 1
+        @test norm(v3) ≈ 1
+        @test E.values == λs
+        @test E.vectors == vs
+        if !(T in oneaxis_types) && VERSION ≥ v"1.2"
+            # If the rotation angle is in [0°, 180°], then the eigvals will be equal.
+            # Note that the randomized RotX (and etc.) have rotation angle in [0°, 360°].
+            # This needs Julia(≥1.2) to get sorted eigenvalues in a canonical order
+            # See https://github.com/JuliaLang/julia/pull/21598
+            @test eigvals(R) ≈ eigvals(collect(R))
+        end
+    end
+end
+
+@testset "Eigen_2D" begin
+    all_types = (RotMatrix{2}, Angle2d)
+
+    @testset "$(T)" for T in all_types, θ in 0.0:0.1:π
+        R = T(Angle2d(θ))
+        λs = eigvals(R)
+        vs = eigvecs(R)
+        E = eigen(R)
+        v1 = vs[:,1]
+        v2 = vs[:,2]
+        @test R * vs ≈ transpose(λs) .* vs
+        @test norm(v1) ≈ 1
+        @test norm(v2) ≈ 1
+        @test E.values == λs
+        @test E.vectors == vs
+        if VERSION ≥ v"1.2"
+            # If the rotation angle θ is in [0°, 180°], then the eigvals will be equal.
+            # Note that the randomized RotX (and etc.) have rotation angle in [0°, 360°].
+            # This needs Julia(≥1.2) to get sorted eigenvalues in a canonical order
+            # See https://github.com/JuliaLang/julia/pull/21598
+            @test eigvals(R) ≈ eigvals(collect(R))
+        end
+    end
+end
