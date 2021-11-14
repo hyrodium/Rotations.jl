24bef254f3aa1bd0eb8dd49f71d5ead063f2e2f5
hyrodium <hyrodium@gmail.com>
2021-11-14 12:37:00 +0900

    update log with InfinitesimalRotation

--- src/log.jl
+++ src/log.jl
@@ -1,9 +1,8 @@
+## log
 # 3d
 function Base.log(R::RotationVec)
     x, y, z = params(R)
-    return @SMatrix [0 -z  y
-                     z  0 -x
-                    -y  x  0]
+    return InfinitesimalRotationVec(x,y,z)
 end
 
 function Base.log(R::Rotation{3})
@@ -14,10 +13,30 @@ end
 # 2d
 function Base.log(R::Angle2d)
     θ, = params(R)
-    return @SMatrix [0 -θ
-                     θ  0]
+    return InfinitesimalAngle2d(θ)
 end
 
 function Base.log(R::Rotation{2})
     log(Angle2d(R))
+end
+
+
+## exp
+# 3d
+function Base.exp(R::InfinitesimalRotationVec)
+    return RotationVec(R.x,R.y,R.z)
+end
+
+function Base.exp(R::InfinitesimalRotation{3})
+    exp(InfinitesimalRotationVec(R))
+end
+
+
+# 2d
+function Base.exp(R::InfinitesimalAngle2d)
+    return Angle2d(R.v)
+end
+
+function Base.exp(R::InfinitesimalRotation{2})
+    exp(InfinitesimalAngle2d(R))
 end
