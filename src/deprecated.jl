e5d214ee6d699208f760135f31536b82c2a6616d
hyrodium <hyrodium@gmail.com>
2021-11-16 22:50:39 +0900

    deprecate UnitQuaternion

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -5,5 +5,8 @@ Base.@deprecate_binding RodriguesVec RotationVec true
 # Deprecate Quat => QuatRotation
 Base.@deprecate_binding Quat QuatRotation true
 
+# Deprecate UnitQuaternion => QuatRotation
+Base.@deprecate_binding UnitQuaternion QuatRotation true
+
 # Deprecate SPQuat => MRP
 Base.@deprecate_binding SPQuat MRP true
--- test/deprecated.jl
+++ test/deprecated.jl
@@ -3,5 +3,6 @@
     # RodriguesVec
     @test Base.isdeprecated(Rotations, :RodriguesVec)
     @test Base.isdeprecated(Rotations, :Quat)
+    @test Base.isdeprecated(Rotations, :UnitQuaternion)
     @test Base.isdeprecated(Rotations, :SPQuat)
 end
