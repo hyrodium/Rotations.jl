37b3b95a1266899120c6c7227afa37802f75c444
hyrodium <hyrodium@gmail.com>
2021-11-17 17:12:26 +0900

    remove old deprecation

--- test/deprecated.jl
+++ test/deprecated.jl
@@ -1,8 +1,5 @@
 
 @testset "Deprecations" begin
     # RodriguesVec
-    @test Base.isdeprecated(Rotations, :RodriguesVec)
-    @test Base.isdeprecated(Rotations, :Quat)
     @test Base.isdeprecated(Rotations, :UnitQuaternion)
-    @test Base.isdeprecated(Rotations, :SPQuat)
 end
