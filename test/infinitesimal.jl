500a2d9f3dc6fe51ba8e4692b15edc9a8a504870
hyrodium <hyrodium@gmail.com>
2021-11-20 22:09:56 +0900

    update tests

--- test/infinitesimal.jl
+++ test/infinitesimal.jl
@@ -77,13 +77,19 @@
         for (T, N) in ((InfinitesimalAngle2d, 2), (InfinitesimalRotationVec, 3))
             R = InfinitesimalRotMatrix
 
-            @test zero(T)      + zero(R{N})        isa R{N, Float64}
-            @test zero(T{Int}) + zero(R{N,Int})    isa R{N, Int}
-            @test zero(T{Int}) + zero(T{BigFloat}) isa T{BigFloat}
+            @test zero(T)           + zero(R{N})        isa R{N, Float64}
+            @test zero(R{T})        + zero(T)           isa R{N, Float64}
+            @test zero(T{Int})      + zero(R{N,Int})    isa R{N, Int}
+            @test zero(R{N,Int})    + zero(T{Int})      isa R{N, Int}
+            @test zero(T{Int})      + zero(T{BigFloat}) isa T{BigFloat}
+            @test zero(T{BigFloat}) + zero(T{Int})      isa T{BigFloat}
 
-            @test zero(T)      - zero(R{N})        isa R{N, Float64}
-            @test zero(T{Int}) - zero(R{N,Int})    isa R{N, Int}
-            @test zero(T{Int}) - zero(T{BigFloat}) isa T{BigFloat}
+            @test zero(T)           - zero(R{N})        isa R{N, Float64}
+            @test zero(R{T})        - zero(T)           isa R{N, Float64}
+            @test zero(T{Int})      - zero(R{N,Int})    isa R{N, Int}
+            @test zero(R{N,Int})    - zero(T{Int})      isa R{N, Int}
+            @test zero(T{Int})      - zero(T{BigFloat}) isa T{BigFloat}
+            @test zero(T{BigFloat}) - zero(T{Int})      isa T{BigFloat}
 
             @test 42  * zero(T)      isa T{Float64}
             @test 42  * zero(T{Int}) isa T{Int}
