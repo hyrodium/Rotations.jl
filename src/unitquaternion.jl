b0acafb59cafa221b374b92f1191443b9abfc6b2
hyrodium <hyrodium@gmail.com>
2023-05-10 21:48:56 +0900

    move `rotation_between(::SVector{3}, ::SVector{3})` to `src/rotation_between.jl`

--- src/unitquaternion.jl
+++ src/unitquaternion.jl
@@ -312,22 +312,6 @@ Base.:\(q1::QuatRotation, q2::QuatRotation) = inv(q1)*q2
 Base.:/(q1::QuatRotation, q2::QuatRotation) = q1*inv(q2)
 
 """
-    rotation_between(from, to)
-
-Compute the quaternion that rotates vector `from` so that it aligns with vector
-`to`, along the geodesic (shortest path).
-"""
-function rotation_between(from::SVector{3}, to::SVector{3})
-    # Robustified version of implementation from https://www.gamedev.net/topic/429507-finding-the-quaternion-betwee-two-vectors/#entry3856228
-    normprod = sqrt(dot(from, from) * dot(to, to))
-    T = typeof(normprod)
-    normprod < eps(T) && throw(ArgumentError("Input vectors must be nonzero."))
-    w = normprod + dot(from, to)
-    v = abs(w) < 100 * eps(T) ? perpendicular_vector(from) : cross(from, to)
-    @inbounds return QuatRotation(w, v[1], v[2], v[3]) # relies on normalization in constructor
-end
-
-"""
     slerp(R1::Rotaion{3}, R2::Rotaion{3}, t::Real)
 
 Perform spherical linear interpolation (Slerp) between rotations `R1` and `R2`.
